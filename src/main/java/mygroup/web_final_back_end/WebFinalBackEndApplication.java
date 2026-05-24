e06c494b54ec5565fefc3c690ffe8864ce4b8e3a
RaphaelDiMascio <99277064+RaphaelDiMascio@users.noreply.github.com>
2026-05-24 11:18:59 +0200

    init

---
+++ src/main/java/mygroup/web_final_back_end/WebFinalBackEndApplication.java
@@ -0,0 +1,13 @@
+package mygroup.web_final_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFinalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFinalBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mygroup/web_final_back_end/WebFinalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package mygroup.web_final_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFinalBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
