fbbfa4f9803618d4a6cbb6d06a35e4c05a069fe4
RaphaelDiMascio <99277064+RaphaelDiMascio@users.noreply.github.com>
2026-06-01 12:21:25 +0200

    creation des IDS dans les classes du model

--- src/main/java/mygroup/web_final_back_end/models/SavingsGoal.java
+++ src/main/java/mygroup/web_final_back_end/models/SavingsGoal.java
@@ -1,4 +1,14 @@
 package mygroup.web_final_back_end.models;
 
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "savings_goal")
 public class SavingsGoal {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
 }
--- src/main/java/mygroup/web_final_back_end/models/Transaction.java
+++ src/main/java/mygroup/web_final_back_end/models/Transaction.java
@@ -1,4 +1,15 @@
 package mygroup.web_final_back_end.models;
 
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transaction")
 public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
 }
