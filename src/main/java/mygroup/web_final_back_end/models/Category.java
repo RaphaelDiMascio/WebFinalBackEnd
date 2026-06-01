16641129d7c677d49d62ee3c46c383c375e75ea4
RaphaelDiMascio <99277064+RaphaelDiMascio@users.noreply.github.com>
2026-06-01 11:59:43 +0200

    import of the category class from my blogger app

--- src/main/java/mygroup/web_final_back_end/models/Category.java
+++ src/main/java/mygroup/web_final_back_end/models/Category.java
@@ -1,4 +1,40 @@
 package mygroup.web_final_back_end.models;
 
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", updatable = false, nullable = false)
+	private UUID id;
+
+	@Column(name = "name", unique = true, nullable = false, length = 100)
+	private String name;
+
+	public Category() {
+	}
+
+	public Category(String name) {
+		this.name = name;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
