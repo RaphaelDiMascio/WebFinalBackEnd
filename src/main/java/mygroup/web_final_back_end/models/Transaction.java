caef9929997b3247228eac9fd7a860a3facda392
RaphaelDiMascio <99277064+RaphaelDiMascio@users.noreply.github.com>
2026-06-01 12:50:27 +0200

    mise en place des transaction

--- src/main/java/mygroup/web_final_back_end/models/Transaction.java
+++ src/main/java/mygroup/web_final_back_end/models/Transaction.java
@@ -1,7 +1,9 @@
 package mygroup.web_final_back_end.models;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -12,4 +14,104 @@ public class Transaction {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	@Column(name = "id", updatable = false, nullable = false)
 	private UUID id;
+
+	@Column(nullable = false)
+	private String name;
+
+	private String description;
+
+	@Column(nullable = false)
+	private Double amount;
+
+	@Column(name = "transaction_date")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+	private LocalDateTime transactionDate;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TransactionType transactionType;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id", nullable = false)
+	private Category category;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Transaction() {}
+
+	public Transaction(String name, String description, Double amount, LocalDateTime transactionDate, TransactionType transactionType, Category category, User user) {
+		this.name = name;
+		this.description = description;
+		this.amount = amount;
+		this.transactionDate = transactionDate;
+		this.transactionType = transactionType;
+		this.category = category;
+		this.user = user;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDateTime getTransactionDate() {
+		return transactionDate;
+	}
+
+	public void setTransactionDate(LocalDateTime transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+
+	public TransactionType getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(TransactionType transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 }
