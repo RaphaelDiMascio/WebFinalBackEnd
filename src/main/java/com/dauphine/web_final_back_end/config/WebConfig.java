9c8a810499d7decf4f24b80f67bcd5d9b2375ca5
RaphaelDiMascio <99277064+RaphaelDiMascio@users.noreply.github.com>
2026-06-04 23:07:03 +0200

    security fix

--- src/main/java/com/dauphine/web_final_back_end/config/WebConfig.java
+++ src/main/java/com/dauphine/web_final_back_end/config/WebConfig.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // Toutes les routes de l'API
-				.allowedOrigins("http://localhost:4200") // Uniquement votre frontend
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true);
